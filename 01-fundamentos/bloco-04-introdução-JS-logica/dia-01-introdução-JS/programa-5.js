10a8ad8ea7a23f398422ea9aa698fe911b706508
Arthur <viegas.arthur2013@hotmail.com>
2021-07-28 19:30:18 -0300

    corrige atividade 5

--- 01-fundamentos/bloco-04-introdução-JS-logica/dia-01-introdução-JS/programa-5.js
+++ 01-fundamentos/bloco-04-introdução-JS-logica/dia-01-introdução-JS/programa-5.js
@@ -4,6 +4,8 @@ let angulo3 = 90;
 
 if (angulo1 + angulo2 + angulo3 == 180){
     console.log(true)
+}else if (angulo1 <= 0 || angulo2 <= 0 || angulo3 <= 0){
+    console.log('inválido')
 }else{
     console.log(false)
 }
