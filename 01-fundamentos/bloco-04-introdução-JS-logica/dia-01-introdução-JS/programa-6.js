581e40bfaba1f42f442caa4bd7b69649eb5423f6
Arthur <viegas.arthur2013@hotmail.com>
2021-07-28 19:51:40 -0300

    atividade 6

---
+++ 01-fundamentos/bloco-04-introdução-JS-logica/dia-01-introdução-JS/programa-6.js
@@ -0,0 +1,17 @@
+var peçasDeXadrez = ['peão', 'torre', 'cavalo', 'bispo', 'rainha', 'rei'];
+
+peão = 'O peão movimenta-se apenas uma casa para frente e somente captura outras peças na diagonal. Opcionalmente, cada peão pode avançar duas casas no seu primeiro movimento do jogo.';
+torre = 'A torre movimenta-se pela vertical ou horizontal, mas não pode pular outras peças.';
+cavalo = 'É a única peça que pode pular as outras. O movimento do cavalo é em forma de “L”, quer dizer, duas casas em sentido horizontal e mais uma na vertical ou vice-versa.';
+bispo = 'O bispo move-se ao longo da diagonal. Não pode pular outras peças.';
+rainha = 'A rainha move-se ao longo da horizontal, vertical e diagonais mas não pode pular outras peças.';
+rei = 'O rei pode mover-se em todas as direções (horizontal, vertical e diagonal) somente uma casa de cada vez.';
+
+console.log(peão);
+console.log(torre);
+console.log(cavalo);
+console.log(bispo);
+console.log(rainha);
+console.log(rei);
+
+//nesse caso o console ira retornar uma linha para cada peça.
