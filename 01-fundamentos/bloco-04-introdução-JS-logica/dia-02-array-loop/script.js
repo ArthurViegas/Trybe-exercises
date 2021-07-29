569958ce02ce6cd90f4f4d74f119d9d30b668675
Arthur <viegas.arthur2013@hotmail.com>
2021-07-29 20:25:02 -0300

    atividades do dia 4.1

---
+++ 01-fundamentos/bloco-04-introdução-JS-logica/dia-02-array-loop/script.js
@@ -0,0 +1,63 @@
+
+//  let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//exercicio 1
+// for( let i = 0; i < numbers.length; i+=1){
+//     console.log(numbers[i])
+// }
+
+
+// exercicio 2
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma=0;
+// for(let i=0; i<numbers.length;i+=1){
+//     soma+=numbers[i];
+// }
+// console.log(soma);
+
+
+// exercicio 3
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma=0;
+// for(let i=0; i<numbers.length;i+=1){
+//     soma+=numbers[i];
+// }
+// console.log(soma/numbers.length);
+
+
+// exercicio 4
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma=0;
+// for(let i=0; i<numbers.length;i+=1){
+//     soma+=numbers[i];
+// }
+// let mediaAritmetica= soma/numbers.length;
+// if (mediaAritmetica>20){
+//     console.log('valor maior que 20')
+// }else{
+//     console.log('valor menor que 20')
+// }
+
+
+// exercicio 5 NAO RESOLVIDO
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maiorNumero=0;
+for(let i=0;i>numbers.length;i+=1){
+    for(let x=0;x<i;x+=1){
+        maiorNumero += x>i;
+    }
+}
+
+
+
+// exercicio 6
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let oddNumbers=0;
+
+// for(let i=0;i<numbers.length;i+=1){
+//     if((numbers[i]%2) !== 0){
+//         oddNumbers++;
+        
+//     }
+// }
+// console.log(oddNumbers)
+
