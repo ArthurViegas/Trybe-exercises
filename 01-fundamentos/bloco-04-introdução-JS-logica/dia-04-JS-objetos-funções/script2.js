174ecd9177dff49f43cedac0ca5c581c83a7a69e
Arthur <viegas.arthur2013@hotmail.com>
2021-08-02 20:53:09 -0300

    exercicios pt2

---
+++ 01-fundamentos/bloco-04-introdução-JS-logica/dia-04-JS-objetos-funções/script2.js
@@ -0,0 +1,28 @@
+// exercicio 1
+
+// let palavra = 'arara'
+
+// function verificaPalindrome(){
+//     function reverse(s) {
+//     return s.split('').reverse().join('');
+// }
+
+// if (palavra === reverse(palavra)){
+//     return true;
+// }else{
+//     return false;
+// }
+// }
+// console.log(verificaPalindrome(palavra));
+
+// exercicio 2 inacabado
+
+let arrayTeste = [2, 3, 6, 7, 10, 1];
+
+function maiorIndice(array){
+for(let i = 0; i < array.length; i+=1){
+    console.log(array[i])
+}
+}
+
+maiorIndice(arrayTeste)
