0cf840c6be16e5ab28465eaf6d37b0e39705964c
Arthur <viegas.arthur2013@hotmail.com>
2021-08-02 14:53:07 -0300

    refatoração exercicios 1/2/3

--- 01-fundamentos/bloco-04-introdução-JS-logica/dia-03-JS-logica/script.js
+++ 01-fundamentos/bloco-04-introdução-JS-logica/dia-03-JS-logica/script.js
@@ -1,12 +1,42 @@
-let n=5;
-let string='';
+//exercicio 1
 
-for(let i=0;i<n;i+=1){   
-    for(let x=0;x<n;x+=1){
-        string+='*';
-    }
-string+='\n';
+// let n=5;
+
+// for(let i=0;i<n;i+=1){   
+//   let string='';
+//     for(let x=0;x<n;x+=1){
+//         string+='*';
+//     }
+//   console.log(string);
+// }
+
+
+
+// exercicio 2
+// let n=5;
+
+// for(let i=0;i<=n;i+=1){  
+//   let string=''; 
+//     for(let x=0;x<i;x+=1){
+//         string+='*';
+//     }
+//     console.log(string);
+// }
+
+
+
+// exercicio 3
+
+let n = 5; //variável 1
+for (let i = 1; i <= n; i++) {
+  let string = ""; //criando um contador
+  for (let j = 0; j < n - i; j++) { //criando o segundo
+    string += " ";
+  }   
+for (let k = 0; k < i; k++) {
+  string += "*";
 }
-console.log(string);
+console.log(string); 
+}
 
 
