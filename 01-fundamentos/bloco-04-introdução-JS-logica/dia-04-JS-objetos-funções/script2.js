c9ff63e1c99bf666a4b45cf2f3267d11ba270f73
Arthur <viegas.arthur2013@hotmail.com>
2021-08-03 00:43:58 -0300

    atividade 3

--- 01-fundamentos/bloco-04-introdução-JS-logica/dia-04-JS-objetos-funções/script2.js
+++ 01-fundamentos/bloco-04-introdução-JS-logica/dia-04-JS-objetos-funções/script2.js
@@ -15,36 +15,44 @@
 // }
 // console.log(verificaPalindrome(palavra));
 
+
 // exercicio 2 inacabado
 
-let arrayTeste = [2, 43, 6, 17, 10, 31];
+// let arrayTeste = [2, 3, 6, 7, 10, 1];
 
-function maiorIndice(valor){
-    let maiorValor = 0;
-    let indice = 0;
-    for (let i = 0; i < valor.length  ; i+=1){
-        if (valor[i] > maiorValor) {
-            maiorValor = valor[i]
-            indice = i;
-        }  
-    } 
-    return indice;
-}
-console.log(maiorIndice(arrayTeste));
-
-
-// let array1 = [2, 10, 3, 53, 23, 75, 19, 28, 4, 5];
-
-// function maiorIndiceArray(array){
+// function indiceMaiorValor(valor){
 //     let maiorValor = 0;
-//     let indiceMaiorValor = 0;
-
-//     for(let i = 0; i < array.length; i+=1){
-//         if(array[i] > maiorValor){
-//           maiorValor = array[i];
-//             indiceMaiorValor = i;
-//         }
-//     }
-//     return indiceMaiorValor;
+//     let indice = 0;
+//     for (let i = 0; i < valor.length  ; i+=1){
+//         if (valor[i] > maiorValor) {
+//             maiorValor = valor[i]
+//             indice = i;
+//         }  
+//     } 
+//     return indice;
 // }
-// console.log(maiorIndiceArray(array1));
+// console.log indiceMaiorValor(arrayTeste));
+
+
+// exercicio 3
+
+let arrayTeste = [12, 4, 6, 7, 10, 1, 23, -2];
+
+function indiceMenorValor(array){
+    // let valoresAnalisados=[];
+    let indice=0;
+    for (let i = 0; i < array.length; i+=1) {
+          if(array[i] < array.length){
+            menorValor = array[i];
+            indice = i;
+            // valoresAnalisados.push(array[i])  
+        
+        }
+        
+    }
+//    console.log(valoresAnalisados);
+   console.log(indice);
+
+}
+
+indiceMenorValor(arrayTeste)
