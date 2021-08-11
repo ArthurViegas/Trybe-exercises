e2042bfddafb06bd3598ba3decdf128fa763c550
Arthur <viegas.arthur2013@hotmail.com>
2021-08-11 13:39:24 -0300

    exercicio 3

--- 01-fundamentos/bloco-05-JS-SOM-eventos-e-web-storage/dia-02-trabalhando-com-elementos/exercicios/script.js
+++ 01-fundamentos/bloco-05-JS-SOM-eventos-e-web-storage/dia-02-trabalhando-com-elementos/exercicios/script.js
@@ -2,3 +2,15 @@ let titleH1 = document.createElement('h1')
     titleH1.innerText = 'Exercício 5.2 - JavaScript DOM'
 
     document.body.appendChild(titleH1)
+
+let mainTag = document.createElement('main')
+    mainTag.className = 'main-content'
+
+    document.body.appendChild(mainTag)
+
+let tagMain = document.getElementsByClassName('main-content');
+
+let sectionTag = document.createElement('section')
+    sectionTag.className = 'center-content'
+    
+mainTag.appendChild(sectionTag)
