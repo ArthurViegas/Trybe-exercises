e0e0cfa1febebf39de698768d40a2909fb7477c3
Arthur <viegas.arthur2013@hotmail.com>
2021-08-31 14:31:57 -0300

    pt 1 exercicio 2

--- 01-fundamentos/bloco-07 - Introdução-à-JavaScript-ES6-Testes-Unitários/dia-02-objects/script.js
+++ 01-fundamentos/bloco-07 - Introdução-à-JavaScript-ES6-Testes-Unitários/dia-02-objects/script.js
@@ -1,5 +1,3 @@
-// exercicio 1
-
 const order = {
   name: 'Rafael Andrade',
   phoneNumber: '11-98763-1416',
@@ -35,22 +33,29 @@ const order = {
     total: 60,
   },
 };
+// exercicio 1
 
-const customerInfo = (order) => {
-  const deliveryPerson = order.order.delivery.deliveryPerson
-  const costumer = order.name;
-  const phoneNumber = order.phoneNumber;
-  const street = order.address.street
-  const streetNumber = order.address.number
-  const apartment = order.address.apartment
-  const payment = order.payment.total
-    console.log(`Olá ${deliveryPerson}, entrega para: ${costumer}, Telefone: ${phoneNumber}, R. ${street}, Nº ${streetNumber}, AP: ${apartment}. Valor ficou R$ ${payment},00`);
-}
-customerInfo(order);
-
-// const orderModifier = (order) => {
-//   // Adicione abaixo as informações necessárias.
-
+// const customerInfo = (order) => {
+//   const deliveryPerson = order.order.delivery.deliveryPerson
+//   const costumer = order.name;
+//   const phoneNumber = order.phoneNumber;
+//   const street = order.address.street
+//   const streetNumber = order.address.number
+//   const apartment = order.address.apartment
+//   const payment = order.payment.total
+// console.log(`Olá ${deliveryPerson}, entrega para: ${costumer}, Telefone: ${phoneNumber}, R. ${street}, Nº ${streetNumber}, AP: ${apartment}. Valor ficou R$ ${payment},00`);
 // }
+// customerInfo(order);
 
-// orderModifier(order);
+// exercicio 2
+
+const orderModifier = (order) => {
+  // Adicione abaixo as informações necessárias.
+  const newCostumer = order.name = 'Luiz Silva';
+  const newValue = order.payment.total = '50';
+  const ordered = Object.keys(order.order.pizza);
+  const drink = order.order.drinks.coke.type;
+console.log(`Olá ${newCostumer}, o total do seu pedido de ${ordered[0]} e ${drink} é de R$ ${newValue},00`);
+}
+// "Olá Luiz Silva, o total do seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00."
+orderModifier(order);
