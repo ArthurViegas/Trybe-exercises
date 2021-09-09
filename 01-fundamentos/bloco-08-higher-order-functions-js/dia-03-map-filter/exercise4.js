421e671914b3e4f211df14be94a5b3f3602b0a0e
Arthur <viegas.arthur2013@hotmail.com>
2021-09-09 14:41:47 -0300

    exercicio 4

--- 01-fundamentos/bloco-08-higher-order-functions-js/dia-03-map-filter/exercise4.js
+++ 01-fundamentos/bloco-08-higher-order-functions-js/dia-03-map-filter/exercise4.js
@@ -1,34 +1,5 @@
-const assert = require('assert')
-const expectedResult = [
-  { 
-    id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
-    author: { name: 'George R. R. Martin', birthYear: 1948 },
-    releaseYear: 1991
-  },
-  {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
-    releaseYear: 1954
-  },
-  {
-    id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
-    author: { name: 'Isaac Asimov', birthYear: 1920 },
-    releaseYear: 1951
-  },
-  {
-    id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
-    author: { name: 'Frank Herbert', birthYear: 1920 },
-    releaseYear: 1965
-  }
-];
+const assert = require('assert');
+
 const books = [
   {
     id: 1,
@@ -91,10 +62,33 @@ const books = [
     releaseYear: 1928,
   },
 ];
-function fantasyOrScienceFiction() {
-  return expectedResult.filter((genero) => (
-    genero.genre === 'Fantasia'|| genero.genre === 'Ficção Científica'
-    ));
-}
-console.log(fantasyOrScienceFiction());
-assert.deepStrictEqual(fantasyOrScienceFiction(), expectedResult);
+const expectedResult = [
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+];
+
+function oldBooksOrdered() {
+ return books.filter((book) => (
+  2020 - book.releaseYear >= 60
+ )).sort((book1, book2) => book1.releaseYear - book2.releaseYear)}
+console.log(oldBooksOrdered());
+// assert.deepStrictEqual(oldBooksOrdered(), expectedResult);
