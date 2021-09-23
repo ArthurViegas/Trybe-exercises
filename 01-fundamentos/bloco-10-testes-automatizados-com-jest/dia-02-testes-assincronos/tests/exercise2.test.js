fb7e6a8907edfe34074fd0bdfcef0fd9e833f61f
Arthur <viegas.arthur2013@hotmail.com>
2021-09-23 14:11:06 -0300

    exercicio 2 adicionando teste de erro id inválido

--- 01-fundamentos/bloco-10-testes-automatizados-com-jest/dia-02-testes-assincronos/tests/exercise2.test.js
+++ 01-fundamentos/bloco-10-testes-automatizados-com-jest/dia-02-testes-assincronos/tests/exercise2.test.js
@@ -5,3 +5,9 @@ test('Testa se ao passar o parametro "1" retorna o usuario "Mark"', () => (
     expect(user).toEqual('Mark')
   })
 ))
+
+test('Testa se ao passar o parametro "3" retorna um erro', () => {
+  expect.assertions(1);
+  return getUserName(3).catch((error) => 
+  expect(error.message).toMatch('User with 3 not found.')
+)})
