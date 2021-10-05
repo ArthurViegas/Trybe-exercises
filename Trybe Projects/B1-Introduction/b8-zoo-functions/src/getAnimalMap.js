1da79330c974b8319f8387603ad3192ed10464b7
Arthur <viegas.arthur2013@hotmail.com>
2021-10-05 14:50:22 -0300

    add zoo-functions project

---
+++ Trybe Projects/B1-Introduction/b8-zoo-functions/src/getAnimalMap.js
@@ -0,0 +1,23 @@
+const data = require('../data/zoo_data');
+
+function getAnimalMap(options) {
+  // const animalMap = {
+  //   NE: [],
+  //   NW: [],
+  //   SE: [],
+  //   SW: [],
+  // }
+  // if (!options) {
+  //   const animals = species.forEach((animal) => {
+  //     if (animal.location === 'NE') animalMap.NE.push(animal.name);
+  //     if (animal.location === 'NW') animalMap.NW.push(animal.name);
+  //     if (animal.location === 'SE') animalMap.SE.push(animal.name);
+  //     if (animal.location === 'SW') animalMap.SW.push(animal.name);
+  //   });
+  //   return animalMap;
+  // }
+}
+
+// console.log(getAnimalMap({ includeNames: true }));
+
+module.exports = getAnimalMap;
