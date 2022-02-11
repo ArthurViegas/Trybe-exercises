42194c863e7198ed44ed7657dcf4b2d3c65eb7f1
Arthur <viegas.arthur2013@hotmail.com>
2022-02-11 15:29:41 -0300

    exercicio 3

--- 03-Desenvolvimento-Back-End/Bloco-22-introdução-desenvolvimento-Web-com-Node/dia-02-fluxo-assincrono/exercicio2.js
+++ 03-Desenvolvimento-Back-End/Bloco-22-introdução-desenvolvimento-Web-com-Node/dia-02-fluxo-assincrono/exercicio2.js
@@ -1,9 +1,13 @@
 const calcule = require('./exercicio1')
 const randon = () => Math.floor(Math.random() * 100 + 1)
 
-// função randomNumbers tirada do gabarito do exercicio
-const randomNumbers = Array.from({ length: 3 }).map(randon)
-
-calcule(...randomNumbers)
-  .then(resolve => console.log(resolve))
-  .catch(error => console.log(error))
+async function doCalcule() {
+  const randomNumbers = Array.from({ length: 3 }).map(randon)
+  try {
+    const request = await calcule(...randomNumbers)
+    console.log(request);
+  } catch (error) {
+    console.log(error);
+  }
+} 
+doCalcule()
