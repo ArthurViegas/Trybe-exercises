740e36fd6a7f22bcea65f56db35d24abe0ff69a6
Arthur <viegas.arthur2013@hotmail.com>
2022-02-11 19:41:26 -0300

    exercicio 2

--- 03-Desenvolvimento-Back-End/Bloco-22-introdução-desenvolvimento-Web-com-Node/dia-04-http-com-node/index.js
+++ 03-Desenvolvimento-Back-End/Bloco-22-introdução-desenvolvimento-Web-com-Node/dia-04-http-com-node/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/ping', (_req, res) => {
-  res.json({message: 'pong'})
+  res.json({"message": 'pong'})
 })
 
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.status(200).json({"message": `Hello, ${nome}!`})
+})
 
 app.listen(3000, () => console.log('alo alo teste'))
